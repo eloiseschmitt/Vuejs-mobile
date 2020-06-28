5cecd35855583632981fdddefa35b6c192d6d6a1
Eloïse Schmitt <eloiseschmitt@iMac-de-Eloise.local>
2020-06-28 16:07:35 +0200

    pages and routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,26 +1,31 @@
 import Vue from 'vue'
-import VueRouter, { RouteConfig } from 'vue-router'
-import Home from '../views/Home.vue'
+import Router, { RouteConfig } from 'vue-router'
 
-Vue.use(VueRouter)
+import TopStories from '../views/TopStories.vue';
+import CodeExamples from '../views/CodeExamples.vue';
+import MyFavorites from '../views/MyFavorites.vue';
 
-  const routes: Array<RouteConfig> = [
+Vue.use(Router)
+
+const routes: Array<RouteConfig> = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'top-stories',
+    component: TopStories
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/code-examples',
+    name: 'code-examples',
+    component: CodeExamples
+  },
+  {
+    path: '/my-favorites',
+    name: 'my-favorites',
+    component: MyFavorites
   }
 ]
 
-const router = new VueRouter({
+const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
